8ffef40614b6094e4d7d50a7eb5f9dc92ae3ee34
guilletdev <guilletdev@gmail.com>
2025-04-09 19:10:10 -0300

    remove layout cn and update lib utils

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-import { cn } from "@/lib/utils";
+
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
